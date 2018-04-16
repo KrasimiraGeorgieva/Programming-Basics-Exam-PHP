c7a431dc2c4b9133fbe757645eab612fdea436be
Krasimira Georgieva <krassi77bg@gmail.com>
2018-04-16 23:53:45 +0300

    Added tasks solutions.

---
+++ Coding 101 Exam - 26 March 2016/05. Butterfly.php
@@ -0,0 +1,24 @@
+<?php
+
+$n = intval(readline());
+$halfRows = $n - 2;
+$dashes = str_repeat('-', $halfRows);
+$stars = str_repeat('*', $halfRows);
+$spaces = str_repeat(' ', $halfRows + 1);
+
+for ($i = 1; $i <= $halfRows; $i++) {
+    if ($i % 2 == 0) {
+        echo "$dashes\\" . ' ' . "/$dashes\n";
+    } else {
+        echo "$stars\\" . ' ' . "/$stars\n";
+    }
+}
+echo "$spaces" . '@' . "$spaces" . "\n";
+
+for ($i = 1; $i <= $halfRows; $i++) {
+    if ($i % 2 == 0) {
+        echo "$dashes/" . ' ' . "\\$dashes\n";
+    } else {
+        echo "$stars/" . ' ' . "\\$stars\n";
+    }
+}
