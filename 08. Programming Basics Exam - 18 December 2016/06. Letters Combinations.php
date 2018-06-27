53d7b60d5306f78b9a54f73c995818b13e63d2a5
Krasimira Georgieva <krassi77bg@gmail.com>
2018-06-27 22:35:18 +0300

    Update solution. 100/100

--- 08. Programming Basics Exam - 18 December 2016/06. Letters Combinations.php
+++ 08. Programming Basics Exam - 18 December 2016/06. Letters Combinations.php
@@ -1,14 +1,14 @@
 <?php
 
-$first = readline();
-$second = readline();
-$third = readline();
+$first = ord(readline());
+$second = ord(readline());
+$third = ord(readline());
 $count = 0;
 for ($i = $first; $i <= $second; $i++) {
     for ($i1 = $first; $i1 <= $second; $i1++) {
         for ($i2 = $first; $i2 <= $second; $i2++) {
             if ($i != $third && $i1 != $third && $i2 != $third) {
-                print ($i . $i1 . $i2 . " ");
+                print (chr($i) . chr($i1) . chr($i2) . " ");
                 $count++;
             }
         }
